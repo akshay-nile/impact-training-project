44c9b97f7886ef1407f96c518d3cfc258a5c9781
Akshay Nile <akshaynile8849@gmail.com>
2022-04-09 22:33:38 +0530

    renamed project folders to lowercase

--- Backend/start-all.cmd
+++ Backend/start-all.cmd
@@ -1,10 +1,10 @@
-start cmd @cmd /k "cd ./eureka && mvn spring-boot:run"
-start cmd @cmd /k "cd ./hospital && mvn spring-boot:run"
-start cmd @cmd /k "cd ./admin && mvn spring-boot:run"
+start cmd @cmd /k "cd ./eureka && mvn spring-boot:run && exit"
 
-start cmd @cmd /k "cd ./NotesService && mvn spring-boot:run"
-start cmd @cmd /k "cd ./AppointmentService && mvn spring-boot:run"
-start cmd @cmd /k "cd ./patient-visits && mvn spring-boot:run"
-start cmd @cmd /k "cd ./authentication && mvn spring-boot:run"
+start cmd @cmd /k "cd ./hospital && mvn spring-boot:run && exit"
+start cmd @cmd /k "cd ./admin && mvn spring-boot:run && exit"
+start cmd @cmd /k "cd ./notes && mvn spring-boot:run && exit"
+start cmd @cmd /k "cd ./appointments && mvn spring-boot:run && exit"
+start cmd @cmd /k "cd ./patient-visits && mvn spring-boot:run && exit"
+start cmd @cmd /k "cd ./authentication && mvn spring-boot:run && exit"
 
-start cmd @cmd /k "cd ./gateway && mvn spring-boot:run"
+start cmd @cmd /k "cd ./gateway && mvn spring-boot:run && exit"
