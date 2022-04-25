0f13995a0bd3cbf1149f559667c681a5f7904c59
Tejas Gaikar <TejasG2@citiustech.com>
2022-04-25 16:07:00 +0530

    Added admin dashboard

--- Frontend/hospital/src/app/modules/admin/admin-routing.module.ts
+++ Frontend/hospital/src/app/modules/admin/admin-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DataManagementComponent } from './components/data-management/data-management.component';
+import { PatientManagementComponent } from './components/patient-management/patient-management.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { UserManagementComponent } from './components/user-management/user-management.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent }
+  {
+    path: 'dashboard', component: DashboardComponent, children: [
+      { path: '', redirectTo: 'user-management', pathMatch: 'full' },
+      { path: 'user-management', component: UserManagementComponent },
+      { path: 'patient-management', component: PatientManagementComponent },
+      { path: 'data-management', component: DataManagementComponent },
+      { path: 'profile', component: ProfileComponent }
+    ]
+  }
 ];
 
 @NgModule({
