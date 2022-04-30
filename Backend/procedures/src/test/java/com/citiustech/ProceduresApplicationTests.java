7958981dd632deb446d204acef2cae4a8aa908b6
Akshay Nile <akshaynile8849@gmail.com>
2022-04-30 19:00:08 +0530

    stuck in docker

--- Backend/procedures/src/test/java/com/citiustech/ProceduresApplicationTests.java
+++ Backend/procedures/src/test/java/com/citiustech/ProceduresApplicationTests.java
@@ -1,12 +1,10 @@
 package com.citiustech;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProceduresApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
