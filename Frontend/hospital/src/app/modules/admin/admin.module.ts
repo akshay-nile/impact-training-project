bd825a7de4559a2e87816c7bd67a4c2381a987ce
Akshay Nile <akshaynile8849@gmail.com>
2022-11-06 10:08:39 +0530

    resolved vulnerabilities and supressed compilation warnings

--- Frontend/hospital/src/app/modules/admin/admin.module.ts
+++ Frontend/hospital/src/app/modules/admin/admin.module.ts
@@ -1,29 +1,29 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/components/shared/shared.module';
+import { MaterialModule } from 'src/app/material.module';
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { StaffManagementComponent } from './components/staff-management/staff-management.component';
+import { AllergyDialogComponent } from './components/data-management/allergy-dialog/allergy-dialog.component';
+import { AllergyTabComponent } from './components/data-management/allergy-tab/allergy-tab.component';
+import { DataManagementComponent } from './components/data-management/data-management.component';
+import { DiagnosisDialogComponent } from './components/data-management/diagnosis-dialog/diagnosis-dialog.component';
+import { DiagnosisTabComponent } from './components/data-management/diagnosis-tab/diagnosis-tab.component';
+import { MedicationDialogComponent } from './components/data-management/medication-dialog/medication-dialog.component';
+import { MedicationTabComponent } from './components/data-management/medication-tab/medication-tab.component';
+import { ProcedureDialogComponent } from './components/data-management/procedure-dialog/procedure-dialog.component';
+import { ProcedureTabComponent } from './components/data-management/procedure-tab/procedure-tab.component';
+import { PatientManagementComponent } from './components/patient-management/patient-management.component';
+import { PatientAllergyViewComponent } from './components/patient-management/patient-view-dialog/patient-allergy-view/patient-allergy-view.component';
+import { PatientDemographicViewComponent } from './components/patient-management/patient-view-dialog/patient-demographic-view/patient-demographic-view.component';
+import { PatientNomineeViewComponent } from './components/patient-management/patient-view-dialog/patient-nominee-view/patient-nominee-view.component';
+import { PatientProfileViewComponent } from './components/patient-management/patient-view-dialog/patient-profile-view/patient-profile-view.component';
+import { PatientViewDialogComponent } from './components/patient-management/patient-view-dialog/patient-view-dialog.component';
 import { EmployeeEditDialogComponent } from './components/staff-management/employee-edit-dialog/employee-edit-dialog.component';
 import { EmployeeRegisterDialogComponent } from './components/staff-management/employee-register-dialog/employee-register-dialog.component';
-import { MaterialModule } from 'src/app/material.module';
-import { SharedModule } from 'src/app/components/shared/shared.module';
-import { PatientManagementComponent } from './components/patient-management/patient-management.component';
-import { PatientViewDialogComponent } from './components/patient-management/patient-view-dialog/patient-view-dialog.component';
-import { PatientProfileViewComponent } from './components/patient-management/patient-view-dialog/patient-profile-view/patient-profile-view.component';
-import { PatientNomineeViewComponent } from './components/patient-management/patient-view-dialog/patient-nominee-view/patient-nominee-view.component';
-import { PatientDemographicViewComponent } from './components/patient-management/patient-view-dialog/patient-demographic-view/patient-demographic-view.component';
-import { PatientAllergyViewComponent } from './components/patient-management/patient-view-dialog/patient-allergy-view/patient-allergy-view.component';
-import { DataManagementComponent } from './components/data-management/data-management.component';
-import { DiagnosisTabComponent } from './components/data-management/diagnosis-tab/diagnosis-tab.component';
-import { DiagnosisDialogComponent } from './components/data-management/diagnosis-dialog/diagnosis-dialog.component';
-import { MedicationTabComponent } from './components/data-management/medication-tab/medication-tab.component';
-import { MedicationDialogComponent } from './components/data-management/medication-dialog/medication-dialog.component';
-import { ProcedureTabComponent } from './components/data-management/procedure-tab/procedure-tab.component';
-import { ProcedureDialogComponent } from './components/data-management/procedure-dialog/procedure-dialog.component';
-import { AllergyTabComponent } from './components/data-management/allergy-tab/allergy-tab.component';
-import { AllergyDialogComponent } from './components/data-management/allergy-dialog/allergy-dialog.component';
+import { StaffManagementComponent } from './components/staff-management/staff-management.component';
 
 
 @NgModule({
--- Frontend/hospital/src/app/modules/patient/patient.module.ts
+++ Frontend/hospital/src/app/modules/patient/patient.module.ts
@@ -1,31 +1,30 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { PatientRoutingModule } from './patient-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PatientVisitDetailsComponent } from './components/patient-visit-details/patient-visit-details.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { PatientRoutingModule } from './patient-routing.module';
 
-
-import { FullCalendarModule } from '@fullcalendar/angular'; 
-import dayGridPlugin from '@fullcalendar/daygrid'; 
-import interactionPlugin from '@fullcalendar/interaction'; 
-import {ScheduleModule} from '@syncfusion/ej2-angular-schedule';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
+import { ScheduleModule } from '@syncfusion/ej2-angular-schedule';
 
-import { AppointmentComponent } from './components/appointment/appointment.component';
-import { PatientVisitHistoryComponent } from './components/patient-visit-history/patient-visit-history.component';
-import { ProfileComponent } from './components/profile/profile.component';
+import { SharedModule } from 'src/app/components/shared/shared.module';
+import { MaterialModule } from 'src/app/material.module';
 import { AppointmentDetailsComponent } from './components/appointment-details/appointment-details.component';
-import { DataCollectionAppointmentComponent } from './components/data-collection-appointment/data-collection-appointment.component';
 import { AppointmentDialogComponent } from './components/appointment-dialog/appointment-dialog.component';
-import { ProfileDetailsComponent } from './components/profile/profile-details/profile-details.component';
+import { AppointmentComponent } from './components/appointment/appointment.component';
+import { DataCollectionAppointmentComponent } from './components/data-collection-appointment/data-collection-appointment.component';
+import { PatientVisitHistoryComponent } from './components/patient-visit-history/patient-visit-history.component';
+import { AllergyDetailsComponent } from './components/profile/allergy-details/allergy-details.component';
 import { DemographicDetailsComponent } from './components/profile/demographic-details/demographic-details.component';
 import { NomineeDetailsComponent } from './components/profile/nominee-details/nominee-details.component';
-import { AllergyDetailsComponent } from './components/profile/allergy-details/allergy-details.component';
-import { MaterialModule } from 'src/app/material.module';
-import { SharedModule } from 'src/app/components/shared/shared.module';
+import { ProfileDetailsComponent } from './components/profile/profile-details/profile-details.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 FullCalendarModule.registerPlugins([ // register FullCalendar plugins
   dayGridPlugin,
